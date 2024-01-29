a44465b647d933867e2049151e36ab8623a2a04b
domica14 <carlosreniel135@gmail.com>
2024-01-28 20:40:54 -0500

    Agregado de pantalla inicial

--- app/src/main/java/com/example/matematica/MainActivity.java
+++ app/src/main/java/com/example/matematica/MainActivity.java
@@ -1,13 +1,36 @@
 package com.example.matematica;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
+
+    //Declaracion de variables para manejar los elementos de la vista
+    private Button btnEmpezar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Asociacion de la variable declarada con el elemento de la vista
+        btnEmpezar = findViewById(R.id.btnEmpezar);
+
+        btnEmpezar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                fade(btnEmpezar);
+            }
+        });
+    }
+
+    //Metodo que se encarga de la transicion entre activities
+    public void fade(View button){
+        //Metodo para realizar el cambio de activity
+        startActivity(new Intent(MainActivity.this, SeleccionDificultad.class));
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);         //Metodo para cambiar la transicion
     }
 }
---
+++ app/src/main/java/com/example/matematica/SeleccionDificultad.java
@@ -0,0 +1,13 @@
+package com.example.matematica;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SeleccionDificultad extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_seleccion_dificultad);
+    }
+}

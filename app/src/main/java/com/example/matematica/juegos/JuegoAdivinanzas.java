68e62f64f49945901851e7c07f351c0200bc9709
domica14 <carlosreniel135@gmail.com>
2024-02-19 20:37:21 -0500

    Finalizacion de juego Tic-Tac-Toe. Tambien se creo la activity para el segundo juego

---
+++ app/src/main/java/com/example/matematica/juegos/JuegoAdivinanzas.java
@@ -0,0 +1,14 @@
+package com.example.matematica.juegos;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import com.example.matematica.R;
+
+public class JuegoAdivinanzas extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_juego_adivinanzas);
+    }
+}
--- app/src/main/java/com/example/matematica/juegos/JuegoX0.java
+++ app/src/main/java/com/example/matematica/juegos/JuegoX0.java
@@ -27,7 +27,7 @@ public class JuegoX0 extends AppCompatActivity implements View.OnClickListener {
     private Set<Integer> contrincanteOpciones = new HashSet<>();
 
     //Lista que contiene las combinanciones de respuestas
-    private Integer[][] combinaciones = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 5, 9}, {3, 5, 7}, {1, 4, 7},
+    private int[][] combinaciones = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {1, 5, 9}, {3, 5, 7}, {1, 4, 7},
                                      {2, 5, 8}, {3, 6, 9}};
 
     @Override
@@ -84,7 +84,7 @@ public class JuegoX0 extends AppCompatActivity implements View.OnClickListener {
     public void onClick(View v) {
         //Boton de salir
         int id = v.getId();
-        juego(v, id);
+        juego(id);
         if (id == R.id.btnSalir) {
             startActivity(new Intent(JuegoX0.this, SeleccionUnidad.class));
             fade();
@@ -93,173 +93,155 @@ public class JuegoX0 extends AppCompatActivity implements View.OnClickListener {
     }
 
     //Metodo que maneja el funcionamiento del juego por la parte del contrincante
-    public void juego(View v, int id){
+    public void juego(int id){
 
+        //Turno usuario
+        if (usuarioOpciones.size() == contrincanteOpciones.size()){
+            turnoUsuario(id);
+        }
+        //Turno contrincante
+        if (contrincanteOpciones.size() < usuarioOpciones.size() && usuarioOpciones.size() <= 5){
+            //Para evitar mas turnos de los posibles se establece el limite
+            if (usuarioOpciones.size() == 5){
+                txtResultado.setText("Empate :/");
+            } else {            //En caso de no haber llegado al maximo se ejecuta todo normalmente
+                turnoContrincante();
+            }
+        }
+        //Confirma que se hayan seleccionado un minimo de tres opciones para verificar el ganador
+            if (buscarGanador(usuarioOpciones)){
+                txtResultado.setText("Ganaste!");
+            } else if (buscarGanador(contrincanteOpciones)) {
+                txtResultado.setText("Perdiste :(");
+            }
+    }
+
+    //Metodo que maneja el funcionamiento del turno del usuario
+    public void turnoUsuario(int id){
+        //Primera fila
+        if (id == R.id.cuadro1) {
+            //Se reemplaza la imagen por la que contiene el O
+            cuadro1.setImageResource(R.drawable.tresenrayao);
+            cuadro1.setEnabled(false);          //Se desactiva el boton
+            usuarioOpciones.add(1);
+        } else if (id == R.id.cuadro2) {
+            cuadro2.setImageResource(R.drawable.tresenrayao);
+            cuadro2.setEnabled(false);          //Se desactiva el boton
+            usuarioOpciones.add(2);
+        } else if (id == R.id.cuadro3) {
+            cuadro3.setImageResource(R.drawable.tresenrayao);
+            cuadro3.setEnabled(false);          //Se desactiva el boton
+            usuarioOpciones.add(3);
+            //Segunda fila
+        } else if (id == R.id.cuadro4) {
+            cuadro4.setImageResource(R.drawable.tresenrayao);
+            cuadro4.setEnabled(false);          //Se desactiva el boton
+            usuarioOpciones.add(4);
+        } else if (id == R.id.cuadro5) {
+            cuadro5.setImageResource(R.drawable.tresenrayao);
+            cuadro5.setEnabled(false);          //Se desactiva el boton
+            usuarioOpciones.add(5);
+        } else if (id == R.id.cuadro6) {
+            cuadro6.setImageResource(R.drawable.tresenrayao);
+            cuadro6.setEnabled(false);          //Se desactiva el boton
+            usuarioOpciones.add(6);
+            //Tercera fila
+        } else if (id == R.id.cuadro7) {
+            cuadro7.setImageResource(R.drawable.tresenrayao);
+            cuadro7.setEnabled(false);          //Se desactiva el boton
+            usuarioOpciones.add(7);
+        } else if (id == R.id.cuadro8) {
+            cuadro8.setImageResource(R.drawable.tresenrayao);
+            cuadro8.setEnabled(false);          //Se desactiva el boton
+            usuarioOpciones.add(8);
+        } else if (id == R.id.cuadro9) {
+            cuadro9.setImageResource(R.drawable.tresenrayao);
+            cuadro9.setEnabled(false);          //Se desactiva el boton
+            usuarioOpciones.add(9);
+        }
+    }
+
+    //Metodo que maneja el funcionamiento del turno del contrincante
+    public void turnoContrincante(){
         //Se crea una instancia de la clase Random
         Random random = new Random();
 
         //Definicion de limites de los numeros aleatorios
         int min = 1;
         int max = 9;
-        if (usuarioOpciones.size() == contrincanteOpciones.size()){
-            id = v.getId();
-            //Primera fila
-            if (id == R.id.cuadro1) {
-                //Se reemplaza la imagen por la que contiene el O
-                cuadro1.setImageResource(R.drawable.tresenrayao);
-                cuadro1.setEnabled(false);          //Se desactiva el boton
-                usuarioOpciones.add(1);
-            } else if (id == R.id.cuadro2) {
-                cuadro2.setImageResource(R.drawable.tresenrayao);
-                cuadro2.setEnabled(false);          //Se desactiva el boton
-                usuarioOpciones.add(2);
-            } else if (id == R.id.cuadro3) {
-                cuadro3.setImageResource(R.drawable.tresenrayao);
-                cuadro3.setEnabled(false);          //Se desactiva el boton
-                usuarioOpciones.add(3);
-            //Segunda fila
-            } else if (id == R.id.cuadro4) {
-                cuadro4.setImageResource(R.drawable.tresenrayao);
-                cuadro4.setEnabled(false);          //Se desactiva el boton
-                usuarioOpciones.add(4);
-            } else if (id == R.id.cuadro5) {
-                cuadro5.setImageResource(R.drawable.tresenrayao);
-                cuadro5.setEnabled(false);          //Se desactiva el boton
-                usuarioOpciones.add(5);
-            } else if (id == R.id.cuadro6) {
-                cuadro6.setImageResource(R.drawable.tresenrayao);
-                cuadro6.setEnabled(false);          //Se desactiva el boton
-                usuarioOpciones.add(6);
-            //Tercera fila
-            } else if (id == R.id.cuadro7) {
-                cuadro7.setImageResource(R.drawable.tresenrayao);
-                cuadro7.setEnabled(false);          //Se desactiva el boton
-                usuarioOpciones.add(7);
-            } else if (id == R.id.cuadro8) {
-                cuadro8.setImageResource(R.drawable.tresenrayao);
-                cuadro8.setEnabled(false);          //Se desactiva el boton
-                usuarioOpciones.add(8);
-            } else if (id == R.id.cuadro9) {
-                cuadro9.setImageResource(R.drawable.tresenrayao);
-                cuadro9.setEnabled(false);          //Se desactiva el boton
-                usuarioOpciones.add(9);
+        int finCiclo = 0;   //Variable centinela para terminar el ciclo de generacion de opciones
+
+        //Maneja las opciones que selecciona el contrincante (se generan automaticamente)
+        while (finCiclo == 0){
+            int opcionContrincante = random.nextInt(max - min + 1) + min;
+            //Se verifica que la opcion generada no haya sido seleccionada anteriormente
+            if (!usuarioOpciones.contains(opcionContrincante) && !contrincanteOpciones.contains(opcionContrincante)){
+                switch (opcionContrincante){
+                    case 1:
+                        cuadro1.setImageResource(R.drawable.tresenrayax);
+                        cuadro1.setEnabled(false);
+                        contrincanteOpciones.add(1);
+                        break;
+                    case 2:
+                        cuadro2.setImageResource(R.drawable.tresenrayax);
+                        cuadro2.setEnabled(false);
+                        contrincanteOpciones.add(2);
+                        break;
+                    case 3:
+                        cuadro3.setImageResource(R.drawable.tresenrayax);
+                        cuadro3.setEnabled(false);
+                        contrincanteOpciones.add(3);
+                        break;
+                    case 4:
+                        cuadro4.setImageResource(R.drawable.tresenrayax);
+                        cuadro4.setEnabled(false);
+                        contrincanteOpciones.add(4);
+                        break;
+                    case 5:
+                        cuadro5.setImageResource(R.drawable.tresenrayax);
+                        cuadro5.setEnabled(false);
+                        contrincanteOpciones.add(5);
+                        break;
+                    case 6:
+                        cuadro6.setImageResource(R.drawable.tresenrayax);
+                        cuadro6.setEnabled(false);
+                        contrincanteOpciones.add(6);
+                        break;
+                    case 7:
+                        cuadro7.setImageResource(R.drawable.tresenrayax);
+                        cuadro7.setEnabled(false);
+                        contrincanteOpciones.add(7);
+                        break;
+                    case 8:
+                        cuadro8.setImageResource(R.drawable.tresenrayax);
+                        cuadro8.setEnabled(false);
+                        contrincanteOpciones.add(8);
+                        break;
+                    case 9:
+                        cuadro9.setImageResource(R.drawable.tresenrayax);
+                        cuadro9.setEnabled(false);
+                        contrincanteOpciones.add(9);
+                        break;
+                }
+                finCiclo = 1;
             }
         }
-        if (contrincanteOpciones.size() < usuarioOpciones.size()){
-            int finCiclo = 0;   //Variable centinela para terminar el ciclo de generacion de opciones
-            //Maneja las opciones que selecciona el contrincante (se generan automaticamente)
-            while (finCiclo == 0){
-                int opcionContrincante = random.nextInt(max - min + 1) + min;
-                //Se verifica que la opcion generada no haya sido seleccionada anteriormente
-                if (!usuarioOpciones.contains(opcionContrincante) && !contrincanteOpciones.contains(opcionContrincante)){
-                    switch (opcionContrincante){
-                        case 1:
-                            cuadro1.setImageResource(R.drawable.tresenrayax);
-                            cuadro1.setEnabled(false);
-                            contrincanteOpciones.add(1);
-                            break;
-                        case 2:
-                            cuadro2.setImageResource(R.drawable.tresenrayax);
-                            cuadro2.setEnabled(false);
-                            contrincanteOpciones.add(2);
-                            break;
-                        case 3:
-                            cuadro3.setImageResource(R.drawable.tresenrayax);
-                            cuadro3.setEnabled(false);
-                            contrincanteOpciones.add(3);
-                            break;
-                        case 4:
-                            cuadro4.setImageResource(R.drawable.tresenrayax);
-                            cuadro4.setEnabled(false);
-                            contrincanteOpciones.add(4);
-                            break;
-                        case 5:
-                            cuadro5.setImageResource(R.drawable.tresenrayax);
-                            cuadro5.setEnabled(false);
-                            contrincanteOpciones.add(5);
-                            break;
-                        case 6:
-                            cuadro6.setImageResource(R.drawable.tresenrayax);
-                            cuadro6.setEnabled(false);
-                            contrincanteOpciones.add(6);
-                            break;
-                        case 7:
-                            cuadro7.setImageResource(R.drawable.tresenrayax);
-                            cuadro7.setEnabled(false);
-                            contrincanteOpciones.add(7);
-                            break;
-                        case 8:
-                            cuadro8.setImageResource(R.drawable.tresenrayax);
-                            cuadro8.setEnabled(false);
-                            contrincanteOpciones.add(8);
-                            break;
-                        case 9:
-                            cuadro9.setImageResource(R.drawable.tresenrayax);
-                            cuadro9.setEnabled(false);
-                            contrincanteOpciones.add(9);
-                            break;
+    }
+
+    //Metodo encargado de buscar al ganador, se pasa como parametro el set de opciones
+    public boolean buscarGanador(Set opciones){
+        for (int i=0; i<combinaciones.length; i++){     //Se recorren los sub-arrays
+            int coincidencias = 0;              //Contiene la cantidad de coincidencias
+            for (int j=0; j<combinaciones[i].length; j++){      //Se recorren los elementos de los sub-arrays
+                if (opciones.contains(combinaciones[i][j])){     //Se verifica si se contiene el elemento
+                    coincidencias+=1;
+                    if (coincidencias==3){              //Si coinciden tres elementos retorna true
+                        return true;
                     }
-                    finCiclo = 1;
                 }
             }
         }
-        //Confirma que se hayan seleccionado un minimo de tres opciones para verificar el ganador
-        if (usuarioOpciones.size() >= 2){
-            //Linea horizontal 1
-            if (usuarioOpciones.containsAll(Arrays.asList(combinaciones[0]))){    //Verifica que contenga la combinacion
-                txtResultado.setText("Ganaste!");           //Muestra el mensaje con el ganador
-            } else if (contrincanteOpciones.containsAll(Arrays.asList(combinaciones[0]))){
-                txtResultado.setText("Perdiste");
-            }
-            //Linea horizontal 2
-            if (usuarioOpciones.containsAll(Arrays.asList(combinaciones[1]))){
-                txtResultado.setText("Ganaste!");
-            } else if (contrincanteOpciones.containsAll(Arrays.asList(combinaciones[1]))){
-                txtResultado.setText("Perdiste");
-            }
-            //Linea horizontal 3
-            if (usuarioOpciones.containsAll(Arrays.asList(combinaciones[2]))){
-                txtResultado.setText("Ganaste!");
-            } else if (contrincanteOpciones.containsAll(Arrays.asList(combinaciones[2]))){
-                txtResultado.setText("Perdiste");
-            }
-            //Linea diagonal 1
-            if (usuarioOpciones.containsAll(Arrays.asList(combinaciones[3]))){
-                txtResultado.setText("Ganaste!");
-            } else if (contrincanteOpciones.containsAll(Arrays.asList(combinaciones[3]))){
-                txtResultado.setText("Perdiste");
-            }
-            //Linea diagonal 1
-            if (usuarioOpciones.containsAll(Arrays.asList(combinaciones[4]))){
-                txtResultado.setText("Ganaste!");
-            } else if (contrincanteOpciones.containsAll(Arrays.asList(combinaciones[4]))){
-                txtResultado.setText("Perdiste");
-            }
-            //Linea diagonal 2
-            if (usuarioOpciones.containsAll(Arrays.asList(combinaciones[5]))){
-                txtResultado.setText("Ganaste!");
-            } else if (contrincanteOpciones.containsAll(Arrays.asList(combinaciones[5]))){
-                txtResultado.setText("Perdiste");
-            }
-            //Linea vertical 1
-            if (usuarioOpciones.containsAll(Arrays.asList(combinaciones[6]))){
-                txtResultado.setText("Ganaste!");
-            } else if (contrincanteOpciones.containsAll(Arrays.asList(combinaciones[6]))){
-                txtResultado.setText("Perdiste");
-            }
-            //Linea vertical 2
-            if (usuarioOpciones.containsAll(Arrays.asList(combinaciones[7]))){
-                txtResultado.setText("Ganaste!");
-            } else if (contrincanteOpciones.containsAll(Arrays.asList(combinaciones[7]))){
-                txtResultado.setText("Perdiste");
-            }
-            //Linea vertical 3
-            if (usuarioOpciones.containsAll(Arrays.asList(combinaciones[8]))){
-                txtResultado.setText("Ganaste!");
-            } else if (contrincanteOpciones.containsAll(Arrays.asList(combinaciones[8]))){
-                txtResultado.setText("Perdiste");
-            }
-        }
+        return false;    //Retorna false de no alcanzar las 3 coincidencias
     }
 
     /*Se sobrescribe el metodo de onBackPressed para que realize el retroceso a la activity anterior al presionar

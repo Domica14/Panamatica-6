9f044976ed68cc4f44087bb19cb855cfc045a6bd
Admin <adrian.veliz@hotmail.com>
2024-02-21 01:38:59 -0500

    Avance en el modo difícil Se ha creado el ciclo de repetición el cual se efectuara hasta 5 veces, de manera que se generarán operaciones aleatorias que podran ser resueltas por los usuarios, y mostrando al final el resultado que hayan conseguido. Por el momento solo esta disponible en la suma (Modo Difícil).

--- app/src/main/java/com/example/matematica/unidad1/Suma.java
+++ app/src/main/java/com/example/matematica/unidad1/Suma.java
@@ -1,19 +1,29 @@
 package com.example.matematica.unidad1;
 
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.view.Gravity;
+import android.widget.Toast;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 import com.example.matematica.R;
+import com.example.matematica.menu.SeleccionUnidad;
 
 import java.util.Random;
 
 public class Suma extends AppCompatActivity {
 
     TextView txtPregunta, txtResultado, Respuesta;
+
+    Button btnFinal, btnVolver, btnContinuar;
     Button btnVerificar;
-    int respuestaCorrecta;
+    int respuestaCorrecta, Count = 1;
+
+    int respuestasCorrectas = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,8 +34,43 @@ public class Suma extends AppCompatActivity {
         txtResultado = findViewById(R.id.txtResultado);
         Respuesta = findViewById(R.id.Respuesta);
         btnVerificar = findViewById(R.id.btnVerificar);
+        btnFinal = findViewById(R.id.btnFinal);
+        btnVolver = findViewById(R.id.btnVolver);
+        btnContinuar = findViewById(R.id.btnNext);
+        btnFinal.setEnabled(false);
+        btnVolver.setEnabled(false);
+        btnContinuar.setEnabled(false);
 
         generarOperacion();
+
+        txtResultado.setText("Intento: " + Count);
+
+
+        btnFinal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                reiniciarActivity();
+                fade();
+            }
+        });
+
+        btnVolver.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarDialogoConfirmacion();
+            }
+        });
+
+        btnContinuar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(new Intent(Suma.this, Resta.class));
+                fade();
+                finish();
+
+            }
+        });
 
         btnVerificar.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -47,13 +92,87 @@ public class Suma extends AppCompatActivity {
 
     private void verificarRespuesta() {
         String respuestaStr = Respuesta.getText().toString().trim();
+
+        // Verificar si el campo de respuesta no está vacío
         if (!respuestaStr.isEmpty()) {
+            // Incrementar el contador
+            Count++;
+
             int respuestaUsuario = Integer.parseInt(respuestaStr);
+
             if (respuestaUsuario == respuestaCorrecta) {
-                txtResultado.setText("¡Correcto!");
+                mostrarToast("¡Correcto!");
+                respuestasCorrectas++;
             } else {
-                txtResultado.setText("Incorrecto. La respuesta correcta es " + respuestaCorrecta);
+                mostrarToast("Incorrecto. La respuesta correcta es " + respuestaCorrecta);
             }
+
+            if (Count <= 5) {
+                txtResultado.setText("Intento: " + Count);
+                generarOperacion();
+            } else {
+                btnFinal.setEnabled(true);
+                btnFinal.setVisibility(View.VISIBLE);
+                btnVolver.setEnabled(true);
+                btnVolver.setVisibility(View.VISIBLE);
+                btnContinuar.setEnabled(true);
+                btnContinuar.setVisibility(View.VISIBLE);
+                txtPregunta.setVisibility(View.GONE);
+                txtResultado.setVisibility(View.GONE);
+                btnVerificar.setVisibility(View.GONE);
+                Respuesta.setVisibility(View.GONE);
+
+                TextView txtResultadoFinal = findViewById(R.id.txtResultadoFinal);
+                txtResultadoFinal.setVisibility(View.VISIBLE);
+                txtResultadoFinal.setText("Resultado: " + respuestasCorrectas + "/5");
+            }
+
+        } else {
+            // Mostrar un mensaje indicando que el campo de respuesta está vacío
+            mostrarToast("Por favor, ingresa tu respuesta antes de verificar.");
         }
     }
+
+
+    private void mostrarToast(String mensaje) {
+        Toast toast = Toast.makeText(getApplicationContext(), mensaje, Toast.LENGTH_SHORT);
+        toast.setGravity(Gravity.TOP | Gravity.CENTER_HORIZONTAL, 0, 0);
+        toast.show();
+    }
+
+    public void onBackPressed() {
+        mostrarDialogoConfirmacion();
+    }
+
+    public void fade() {
+        overridePendingTransition(R.anim.fade_in, R.anim.fade_out);
+    }
+
+    private void reiniciarActivity() {
+        Intent intent = getIntent();
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP | Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        finish();
+        startActivity(intent);
+    }
+
+    private void mostrarDialogoConfirmacion() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("¿Volver a la selección de unidad y perder el progreso?").setTitle("Confirmación");
+        builder.setPositiveButton("Sí", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                startActivity(new Intent(Suma.this, SeleccionUnidad.class));
+                fade();
+            }
+        });
+        builder.setNegativeButton("No", new DialogInterface.OnClickListener() {
+            public void onClick(DialogInterface dialog, int id) {
+                dialog.dismiss();
+            }
+        });
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
 }
+
+
+
